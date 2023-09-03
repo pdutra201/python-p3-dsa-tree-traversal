ca04709ab9fcb332912596dd99c2a0e8eb885405
philip dutra <monsterphilip10@gmail.com>
2023-09-02 22:51:44 -0400

    completed lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,14 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = []
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        result.append(node)
+        return result[0]
+      nodes_to_visit = nodes_to_visit + node['children']
+    return None
+
